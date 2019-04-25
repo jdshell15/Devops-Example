0ed20f5b26849b0aa17e94cdfac988687f6cf30b
John <jdshell15@gmail.com>
2019-04-25 12:02:28 -0400

    restarting

--- src/test/java/com/revature/test/TodoDaoTest.java
+++ src/test/java/com/revature/test/TodoDaoTest.java
@@ -1,18 +1,16 @@
 package com.revature.test;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import com.revature.dao.TodoDao;
 import com.revature.dao.TodoDaoImpl;
-
-import src.main.java.com.revature.dao.TodoDao;
-import com.revature.dao.TodoDaoImpl;
-
-
-
 
 public class TodoDaoTest {
+
 	private final TodoDao dao = new TodoDaoImpl();
-	
+
 	@Test
 	public void getAllTodos_ShouldReturn1Record() {
 		assertEquals(1, dao.getAllTodos().size());
